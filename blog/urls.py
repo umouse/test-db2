a1d5bb7a2dbac74aede9bd54412fa1a54da73fbd
umouse <umousee@gmail.com>
2017-11-28 11:46:06 +0200

    Fix urls

--- blog/urls.py
+++ blog/urls.py
@@ -24,9 +24,9 @@ from django.contrib.auth.decorators import login_required
 from blog import views
 
 urlpatterns = [
-    url(r'like$', login_required(views.likes_view), name='like'),
-    url(r'posts/(?P<pk>\d+)/details$', login_required(views.PostDetailsView.as_view()), name='post_details'),
-    url(r'posts/(?P<pk>\d+)/comments$',login_required(views.PostCommentsView.as_view()), name='post_comments'),
-    url(r'$', login_required(views.PostListView.as_view()), name='postlist'),
+    url(r'^like$', login_required(views.likes_view), name='like'),
+    url(r'^posts/(?P<pk>\d+)/details$', login_required(views.PostDetailsView.as_view()), name='post_details'),
+    url(r'^posts/(?P<pk>\d+)/comments$',login_required(views.PostCommentsView.as_view()), name='post_comments'),
+    url(r'^$', login_required(views.PostListView.as_view()), name='postlist'),
 
 ]
--- userauth/urls.py
+++ userauth/urls.py
@@ -3,12 +3,12 @@ from django.contrib.auth.views import logout, PasswordChangeDoneView, LoginView
 from userauth import views
 
 urlpatterns = [
-    url(r'login', LoginView.as_view(
+    url(r'^login', LoginView.as_view(
         template_name='login.html',
     ), name='login'),
-    url(r'password_change_done', PasswordChangeDoneView.as_view(), name='password_change_done'),
-    url(r'logout$', logout, {'next_page': '/'}, name='logout'),
-    url(r'sign_up$', view=views.CreateUserView.as_view(),name= 'sign_up'),
-    url(r'send_email$', views.send_login_email, name='send_login_email'),
-    url(r'sign_up_verificated/(.+)$', views.sign_up_varification, name='member_verification'),
+    url(r'^password_change_done', PasswordChangeDoneView.as_view(), name='password_change_done'),
+    url(r'^logout$', logout, {'next_page': '/'}, name='logout'),
+    url(r'^sign_up$', view=views.CreateUserView.as_view(),name= 'sign_up'),
+    url(r'^send_email$', views.send_login_email, name='send_login_email'),
+    url(r'^sign_up_verificated/(.+)$', views.sign_up_varification, name='member_verification'),
 ]
